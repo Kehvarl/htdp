f98e99c4e207a3feb1069681298d35562aa2201b
Kehvarl <kehvarl@kehvarl.com>
2021-04-04 20:04:46 -0700

    htdp 250 abstract out a simplified function

---
+++ III-abstraction/tabulate.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+; Number -> [List-of Number]
+; tabulates sin between n 
+; and 0 (incl.) in a list
+(define (tab-sin n)
+  (cond
+    [(= n 0) (list (sin 0))]
+    [else
+     (cons
+      (sin n)
+      (tab-sin (sub1 n)))]))
+	
+; Number -> [List-of Number]
+; tabulates sqrt between n 
+; and 0 (incl.) in a list
+(define (tab-sqrt n)
+  (cond
+    [(= n 0) (list (sqrt 0))]
+    [else
+     (cons
+      (sqrt n)
+      (tab-sqrt (sub1 n)))]))
+
+(define (tabulate f n)
+  (cond
+    [(= n 0)(list (f 0))]
+    [else
+     (cons
+      (f n)
+      (tabulate f (sub1 n)))]))
