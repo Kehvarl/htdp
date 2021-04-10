b57fed014b96078dd82e14754a6c2ede697e1999
Kehvarl <kehvarl@kehvarl.com>
2021-04-09 19:25:54 -0700

    sort a list of items based on properties of the items

--- III-abstraction/finger167.rkt
+++ III-abstraction/finger167.rkt
@@ -18,3 +18,21 @@
           (define (posn->pair p)
             (cons (posn-x p) (cons (posn-y p) '()))))
     (map posn->pair list-of-posn)))
+
+;[]
+(define-struct inventory [name description acquisition-price sales-price])
+
+(define my-inventory (list
+                       (make-inventory "Item 1" "The third thing" 100.00 150.00)
+                       (make-inventory "Item 2" "Thing 1" 25.00 25.00)
+                       (make-inventory "Item 3" "Secondus" 50.00 75.00)))
+
+(define (sort-inventory list-of-items)
+  (local (
+          (define (price-difference item)
+            (- (inventory-sales-price item) (inventory-acquisition-price item)))
+          (define (compare-items item1 item2)
+            (< (price-difference item1) (price-difference item2))))
+    (sort list-of-items compare-items)))
+
+              
