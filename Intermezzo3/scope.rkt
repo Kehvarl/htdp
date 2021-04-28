ce1c4752b38c60f8dc2fb801a9f51ddad8231252
Kehvarl <kehvarl@kehvarl.com>
2021-04-27 18:28:44 -0700

    update

--- Intermezzo3/scope.rkt
+++ Intermezzo3/scope.rkt
@@ -1,2 +1,15 @@
 #lang htdp/isl+
+(require 2htdp/abstraction)
+
+(define (p1 x y)
+  (+ (* x y)
+     (+ (* 2 x)
+        (+ (* 2 y) 22))))
+ 
+(define (p2 x)
+  (+ (* 55 x) (+ x 11)))
+ 
+(define (p3 x)
+  (+ (p1 x 0)
+     (+ (p1 x 1) (p2 x))))
 
